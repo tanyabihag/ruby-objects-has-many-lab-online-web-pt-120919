dc2cc7c67e818f018965c086230ef1512753a4c2
Tanya Bihag <tbihag@gmail.com>
2020-01-21 01:35:56 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,4 +6,18 @@ def initialize(name)
   @name = name 
 end
 
+def posts 
+  Post.all.select {|post| post.author == self}
 end
+
+def add_post(post)
+  post.author = self 
+end 
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  add_post(post)
+end 
+
+def self.post_count
+  Post.all.length
