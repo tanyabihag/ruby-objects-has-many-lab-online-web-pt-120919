a7df21d36f1c6ded9a573b594972f5b05c18c67a
Tanya Bihag <tbihag@gmail.com>
2020-01-21 01:36:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+
+attr_accessor :name, :artist 
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @@all << self 
+end
+
+def self.all 
+  @@all
+end 
+
+def name 
+  @name 
+end
+
+
+  def artist_name
+    if self.artist.nil?
+      nil
+    else
+      self.artist.name
+    end
+  end
+end
+  
