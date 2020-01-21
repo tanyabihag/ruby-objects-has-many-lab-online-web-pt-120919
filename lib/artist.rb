24fceec0a8f5c795137d0a92253a94871a1a4fff
Tanya Bihag <tbihag@gmail.com>
2020-01-21 01:12:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,18 @@
 class Artist 
   
-attr_accessor :name, :songs
+attr_accessor :name
 
 def initialize(name)
   @name = name 
+  @songs = []
 end
 
 def songs 
-  
+  @songs
 end
+
+def add_songs(song)
+  @songs << song 
+  song.artist = self 
+end 
+
