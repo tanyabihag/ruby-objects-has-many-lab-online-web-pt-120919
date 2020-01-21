0fc8202872b0ff704f43068829b1ce5019fc6841
Tanya Bihag <tbihag@gmail.com>
2020-01-21 00:56:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+
+attr_accessor :name, :artist 
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @@all << self 
+end
+
+def self.all 
+  @@all
+end 
+
+def name 
+  @name 
+end
+
+def artist 
+  
+  
